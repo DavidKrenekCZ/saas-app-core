d4e9252f3b9804721ec47c948243ae50a5d0b503
David Krenek <info@davidkrenek.cz>
2019-09-24 10:34:20 +0200

    Routes refactor - remove web middleware

--- routes/web.php
+++ routes/web.php
@@ -11,34 +11,28 @@
 |
 */
 
-Route::group(['middleware' => 'web'], function () {
     Route::match(['post', 'get'], '/', ['as' => 'homepage', 'uses' => 'AppController@getHomepage']);
     Route::get('/mazlicek/{id}', ['as' => 'detail', 'uses' => 'AppController@getDetail']);
-
     Route::post('/like/{id}', ['as' => 'likePhoto', 'uses' => 'AppController@likePhoto']);
-    Route::get('/upload', ['as' => 'upload', 'uses' => 'AppController@getUpload', 'middleware' => ['auth']]);
-    Route::post('/upload', ['as' => 'upload.process', 'uses' => 'AppController@postUpload', 'middleware' => ['auth']]);
-
-    Route::get('/my-photos', ['as' => 'myPhotos', 'uses' => 'AppController@getMyPhotos', 'middleware' => ['auth']]);
-
-    Route::get('/configure', ['as' => 'configure', 'uses' => 'AppController@getConfigure', 'middleware' => ['auth']]);
-    Route::post('/configure', ['as' => 'configure', 'uses' => 'AppController@postConfigure', 'middleware' => ['auth']]);
-    Route::get('/report', ['as'  => 'report', 'uses' => 'AppController@getReport', 'middleware' => ['auth']]);
-    Route::get('/photo-report', ['as' => 'photoReport', 'uses' => 'AppController@getPhotoReport', 'middleware' => ['auth']]);
-    Route::get('/clear-db', ['as'  => 'clearDb', 'uses' => 'AppController@clearDb', 'middleware' => ['auth']]);
-
     Route::get('/callback', ['as' => 'callback', 'uses' => 'AppController@callback']);
-
     Route::match(['post', 'get'], '/more', ['as' => 'more', 'uses' => 'AppController@getMore']);
     Route::get('/share-only', ['as' => 'share', 'uses' => 'AppController@share']);
     Route::get('/share-image/{image}', ['as' => 'share-image', 'uses' => 'AppController@shareImage']);
-    Route::get('/delete-image/{id}', ['as' => 'delete-image', 'uses' => 'AppController@deleteImage', 'middleware' => ['auth']]);
-
-    Route::get('show-logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-
     Route::get('/cron', ['as' => 'cron', 'uses' => 'AppController@executeCron']);
     Route::get('/top-20', ['as' => 'winners', 'uses' => 'AppController@getWinnersList']);
     Route::get('/test', ['as' => 'test', 'uses' => 'AppController@getTestList']);
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/upload', ['as' => 'upload', 'uses' => 'AppController@getUpload']);
+    Route::post('/upload', ['as' => 'upload.process', 'uses' => 'AppController@postUpload']);
+    Route::get('/my-photos', ['as' => 'myPhotos', 'uses' => 'AppController@getMyPhotos']);
+    Route::get('/configure', ['as' => 'configure', 'uses' => 'AppController@getConfigure']);
+    Route::post('/configure', ['as' => 'configure', 'uses' => 'AppController@postConfigure']);
+    Route::get('/report', ['as'  => 'report', 'uses' => 'AppController@getReport']);
+    Route::get('/photo-report', ['as' => 'photoReport', 'uses' => 'AppController@getPhotoReport']);
+    Route::get('/clear-db', ['as'  => 'clearDb', 'uses' => 'AppController@clearDb']);
+    Route::get('/delete-image/{id}', ['as' => 'delete-image', 'uses' => 'AppController@deleteImage']);
 });
 
 Route::auth();
